d5469c074042ee2d6403780779d5370384e1e17f
BernardoMendess <115937261+BernardoMendess@users.noreply.github.com>
2025-04-26 12:55:33 -0300

    Update funcao1.sql

--- funcao1.sql
+++ funcao1.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE FUNCTION generate_event_ranking(
-    evento_nome VARCHAR,
-    inicio_ranking INT DEFAULT 1,
-    fim_ranking INT DEFAULT 10
+	evento_nome VARCHAR,
+	inicio_ranking INT DEFAULT 1,
+	fim_ranking INT DEFAULT 10
 )
 RETURNS TABLE (
-    posicao_resultado BIGINT,
-    time_resultado VARCHAR,
-    pontos_resultado BIGINT,
-    vitorias_resultado BIGINT,
-    derrotas_resultado BIGINT,
-    media_rounds_resultado NUMERIC,
+	posicao_resultado BIGINT,
+	time_resultado VARCHAR,
+	pontos_resultado BIGINT,
+	vitorias_resultado BIGINT,
+	derrotas_resultado BIGINT,
+	media_rounds_resultado NUMERIC,
 	rounds_ganhos_resultado BIGINT,
 	rounds_perdidos_resultado BIGINT
 ) AS $$
@@ -25,27 +25,27 @@ BEGIN
         WHERE event_name = evento_nome
     ),
     resultados AS (
-        SELECT 
-            time_nome,
-            COUNT(*) FILTER (WHERE resultado > resultado_oponente) AS vitorias,
-            COUNT(*) FILTER (WHERE resultado < resultado_oponente) AS derrotas,
-            ROUND(AVG(resultado + resultado_oponente), 2) AS media_rounds,
-			COUNT(*) FILTER (WHERE resultado > resultado_oponente) * 3 AS pontos,
-			SUM(resultado) AS rounds_ganhos,
-			SUM(resultado_oponente) AS rounds_perdidos
+    SELECT 
+        time_nome,
+        COUNT(*) FILTER (WHERE resultado > resultado_oponente) AS vitorias,
+        COUNT(*) FILTER (WHERE resultado < resultado_oponente) AS derrotas,
+        ROUND(AVG(resultado + resultado_oponente), 2) AS media_rounds,
+	COUNT(*) FILTER (WHERE resultado > resultado_oponente) * 3 AS pontos,
+	SUM(resultado) AS rounds_ganhos,
+	SUM(resultado_oponente) AS rounds_perdidos
         FROM partidas
         GROUP BY time_nome
     ),
     classificacao AS (
         SELECT 
-		    RANK() OVER (ORDER BY pontos DESC, vitorias DESC, media_rounds DESC, time_nome) AS posicao,
+	    RANK() OVER (ORDER BY pontos DESC, vitorias DESC, media_rounds DESC, time_nome) AS posicao,
             time_nome,
             pontos,
             vitorias,
             derrotas,
             media_rounds,
-			rounds_ganhos,
-			rounds_perdidos
+	    rounds_ganhos,
+	    rounds_perdidos
         FROM resultados
     )
     SELECT 
@@ -55,8 +55,8 @@ BEGIN
         c.vitorias,
         c.derrotas,
         c.media_rounds,
-		c.rounds_ganhos,
-		c.rounds_perdidos
+	c.rounds_ganhos,
+	c.rounds_perdidos
     FROM classificacao c
     WHERE c.posicao BETWEEN inicio_ranking AND fim_ranking
     ORDER BY c.posicao;
