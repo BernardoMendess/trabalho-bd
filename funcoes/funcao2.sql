48ed0a6b8d24636bb2aca76ff1b76bb145d87177
Guilherme Pacheco <guilhermepm@id.uff.br>
2025-05-04 22:48:24 -0300

    Update funcao2.sql

--- funcoes/funcao2.sql
+++ funcoes/funcao2.sql
@@ -7,80 +7,69 @@ CREATE OR REPLACE FUNCTION analisar_confrontos(
 )
 RETURNS TABLE (
     mapa_considerado TEXT,
-    total_partidas INTEGER,
+    total_matches INTEGER,
     vitorias_time_a INTEGER,
     vitorias_time_b INTEGER,
-    data_hora_ultimo_confronto TEXT,
-    vencedor_ultima_match TEXT
+    vencedor_ultima_match TEXT,
+    data_hora_ultima_match TEXT
 )
 AS $$
 DECLARE
-    partidas RECORD;
-    partidas_array INT[];
-    total_partidas INT := 0;
-    cont_vitorias_a INT := 0; 
+    partida RECORD;
+    ultima_partida RECORD;
+    cont_vitorias_a INT := 0;
     cont_vitorias_b INT := 0;
-    ultima_match RECORD;
-    data_hora_ultima_match TEXT;
+    total_partidas INT := 0;
+    vencedor_ultima_partida TEXT;
+	data_hora_ultima_partida TEXT;
 BEGIN
-    SELECT array_agg(m.match_id)
-    INTO partidas_array
-    FROM matches AS m
-    INNER JOIN times AS t1 ON t1.time_id = m.team1_id
-    INNER JOIN times AS t2 ON t2.time_id = m.team2_id
-    WHERE (
-        (t1.nome_time = time_a AND t2.nome_time = time_b) OR
-        (t1.nome_time = time_b AND t2.nome_time = time_a)
-    )
-    AND (m.maps = mapa OR mapa IS NULL);
+    FOR partida IN
+        SELECT 
+            m.*,
+            t1.nome_time AS team1_name,
+            t2.nome_time AS team2_name
+        FROM matches m
+        INNER JOIN times t1 ON t1.time_id = m.team1_id
+        INNER JOIN times t2 ON t2.time_id = m.team2_id
+        WHERE (
+            (t1.nome_time = time_a AND t2.nome_time = time_b) OR
+            (t1.nome_time = time_b AND t2.nome_time = time_a)
+        )
+        AND (m.maps = mapa OR mapa IS NULL)
+        ORDER BY m.match_time DESC
+    LOOP
+        total_partidas := total_partidas + 1;
 
-    IF partidas_array IS NULL THEN
-        RETURN QUERY SELECT COALESCE(mapa, 'Todos'), 0, 0, 0, 'Não há', 'Não há';
-        RETURN;
-    END IF;
+        IF total_partidas = 1 THEN
+            ultima_partida := partida;
+        END IF;
 
-    total_partidas := cardinality(partidas_array);
-
-    FOR i IN 1..total_partidas LOOP
-        SELECT m.*, t1.nome_time AS team1_name, t2.nome_time AS team2_name
-        INTO partidas
-        FROM matches AS m
-        JOIN times AS t1 ON t1.time_id = m.team1_id
-        JOIN times AS t2 ON t2.time_id = m.team2_id
-        WHERE m.match_id = partidas_array[i];
-
-        IF (partidas.team1_name = time_a AND partidas.team1_result > partidas.team2_result) OR
-           (partidas.team2_name = time_a AND partidas.team2_result > partidas.team1_result) THEN
+        IF (partida.team1_name = time_a AND partida.team1_result > partida.team2_result) OR
+           (partida.team2_name = time_a AND partida.team2_result > partida.team1_result) THEN
             cont_vitorias_a := cont_vitorias_a + 1;
         ELSE
             cont_vitorias_b := cont_vitorias_b + 1;
         END IF;
     END LOOP;
 
-    SELECT m.*, t1.nome_time AS team1_name, t2.nome_time AS team2_name
-    INTO ultima_match
-    FROM matches m
-    INNER JOIN times AS t1 ON t1.time_id = m.team1_id
-    INNER JOIN times AS t2 ON t2.time_id = m.team2_id
-    WHERE m.match_id = ANY (partidas_array)
-    ORDER BY m.match_time DESC
-    LIMIT 1;
-
-    IF ultima_match.team1_result > ultima_match.team2_result THEN
-        vencedor_ultima_match := ultima_match.team1_name;
+    IF total_partidas = 0 THEN
+        RETURN QUERY SELECT COALESCE(mapa, 'Todos'), 0, 0, 0, 'Não há', 'Nula';
     ELSE
-        vencedor_ultima_match := ultima_match.team2_name;
+	data_hora_ultima_partida := TO_CHAR(ultima_partida.match_time, 'DD/MM/YYYY HH24:MI');
+
+	IF ultima_partida.team1_result > ultima_partida.team2_result THEN
+		vencedor_ultima_partida := ultima_partida.team1_name::TEXT;    
+	ELSE
+		vencedor_ultima_partida := ultima_partida.team2_name::TEXT;   
+	END IF;
+        RETURN QUERY SELECT
+            COALESCE(mapa, 'Todos'),
+            total_partidas,
+            cont_vitorias_a,
+            cont_vitorias_b,
+	    vencedor_ultima_partida,
+            data_hora_ultima_partida;
     END IF;
-
-    data_hora_ultima_match := TO_CHAR(ultima_match.match_time, 'DD/MM/YYYY HH24:MI');
-
-    RETURN QUERY SELECT 
-        COALESCE(mapa, 'Todos'),
-        total_partidas,
-        cont_vitorias_a,
-        cont_vitorias_b,
-        data_hora_ultima_match,
-        vencedor_ultima_match;
 END;
 $$ LANGUAGE plpgsql;
 
